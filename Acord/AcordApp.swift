cefb2269ad7404a7df0c3ed4eaa5fcfbafcd2449
Dorian Zlatan <zlatan.dorian272@gmail.com>
2021-10-06 00:18:54 +0300

    Initial Commit

---
+++ Acord/AcordApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcordApp.swift
+//  Acord
+//
+//  Created by Dorian ZLATAN on 06.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct AcordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
