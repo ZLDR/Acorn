2571b967928d51d6314bead8170e092404ad5042
Dorian Zlatan <zlatan.dorian272@gmail.com>
2021-10-06 22:24:16 +0300

    Finished Page 1

--- Acord/OnboardingPage.swift
+++ Acord/OnboardingPage.swift
@@ -9,29 +9,15 @@ import SwiftUI
 
 
 struct OnboardingPage: View {
-    
     @State var email: String = ""
     @State var password: String = ""
     var body: some View {
         VStack(alignment: .leading, spacing: 24) {
             Group{
                 VStack(alignment: .leading){
-                    HStack {
-                        Image(systemName: "wind")
-                            .foregroundColor(Color(.systemPink))
-                            .font(.system(size: 28))
-                            .aspectRatio(contentMode: .fit)
-                            .scaledToFit()
-                            .frame(width: 50, height: 50)
-                        Spacer()
-                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                            Text("Login")
-                                .foregroundColor(Color(.systemGray))
-                        })
-                    }
-                    //            Spacer()
+                    OnboardingHeading()
                     VStack(alignment: .leading, spacing: 8){
-                        Text("Acorns helps you save & invest")
+                        Text("Winds helps you save & invest")
                             .font(.title)
                             .bold()
                         Text("Easily save & invest in the background of life. invest your spare change, save for retirement, spend smarter, earn more, and grow your knowledge")
@@ -39,37 +25,38 @@ struct OnboardingPage: View {
                             .fontWeight(.light)
                             .lineSpacing(3)
                     }
+                    
                 }
-                Group{
-                    VStack{
-                        Text("Sign up to get started!")
-                            .font(.body)
-                            .fontWeight(.light)
-                    }
-                    VStack{
-                        ZStack{
-                            VStack{
-                                TextInputField("Email",text: $email)
-                            }
-                            .padding(12)
-                            .background(Color(.systemGray6))
-                            .cornerRadius(8)
-
-                        }
-                        ZStack{
-                            VStack{
-                                // Secure field
-                                SecureField("Password", text: $password)
-                            }
-                        }
-                        .padding(12)
-                        .background(Color(.systemGray6))
-                        .cornerRadius(8)
-
-                    }
-                }
+                SignUpForm(email: $email, password: $password)
             }
             Spacer()
+            VStack(alignment: .center, spacing: 24){
+                HStack{
+                    Group{
+                        Circle()
+                            .fill(Color(.black))
+                        
+                        Circle()
+                            .fill(Color(.systemGray4))
+                        Circle()
+                            .fill(Color(.systemGray4))
+                        Circle()
+                            .fill(Color(.systemGray4))
+                        Circle()
+                            .fill(Color(.systemGray4))
+                    }
+                    .frame(width:10, height: 10)
+                }
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("Get started")
+                        .font(.body)
+                        .frame(maxWidth: .infinity, maxHeight: 44)
+                        .background(Color(.systemPink))
+                        .foregroundColor(.white)
+                        .cornerRadius(24)
+                })
+            }
+            .frame(maxWidth: .infinity)
         }
         .padding()
     }
@@ -98,7 +85,86 @@ struct TextInputField: View {
                 .padding(.bottom, 2)
             TextField("Enter your email", text: $text)
                 .font(.subheadline)
-                .foregroundColor(Color(.placeholderText))
+                .foregroundColor(text.isEmpty ? Color(.placeholderText) : Color(.black))
+        }
+    }
+}
+
+
+struct PasswordInputField: View {
+    var title: String
+    @Binding var password: String
+    
+    init(_ title: String, password: Binding<String>) {
+        self.title = title
+        self._password = password
+    }
+    
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 4){
+            Text(title)
+                .font(.footnote)
+                .foregroundColor(Color(.label))
+                .padding(.bottom, 2)
+            SecureField("Create a password", text: $password)
+                .font(.subheadline)
+                .foregroundColor(password.isEmpty ? Color(.placeholderText) : Color(.black))
+        }
+    }
+}
+
+struct OnboardingHeading: View {
+    var body: some View {
+        HStack(alignment: .center) {
+            Image(systemName: "wind")
+                .foregroundColor(Color(.systemPink))
+                .font(.system(size: 28))
+                .aspectRatio(contentMode: .fit)
+                .scaledToFit()
+                .frame(width: 50, height: 50)
+            Spacer()
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Text("Login")
+                    .foregroundColor(Color(.systemGray))
+            })
+        }
+        .padding(.bottom, 12)
+    }
+}
+
+struct SignUpForm: View {
+    @Binding var email: String
+    @Binding var password: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            VStack{
+                Text("Sign up to get started!")
+                    .font(.body)
+                    .fontWeight(.light)
+            }
+            .padding(.bottom, 8)
+            VStack{
+                ZStack{
+                    VStack{
+                        TextInputField("Email",text: $email)
+                    }
+                    .padding(12)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(8)
+                    
+                }
+                ZStack{
+                    VStack{
+                        PasswordInputField("Password", password: $password)
+                    }
+                }
+                .padding(12)
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+                
+            }
         }
     }
 }
